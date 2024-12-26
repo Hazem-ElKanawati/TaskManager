c9bfdcc13cf0c0aa87919020c87e20c6adc412c5
Hazem Mohamed ElKanawati <hazemelkanawati@gmail.com>
2024-12-26 14:09:39 +0200

    Implement PUT and DELETE methods

--- src/main/java/taskmanager/com/Task/Manager/controller/TaskController.java
+++ src/main/java/taskmanager/com/Task/Manager/controller/TaskController.java
@@ -1,6 +1,8 @@
 package taskmanager.com.Task.Manager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import taskmanager.com.Task.Manager.model.Task;
 import taskmanager.com.Task.Manager.service.TaskService;
@@ -24,5 +26,29 @@ public class TaskController {
     {
         return taskService.addTask(task);
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateTask(@PathVariable long id, @RequestBody Task updatedTask)
+    {
+       boolean isUpdated = taskService.updateTask(id , updatedTask);
 
+       if(isUpdated)
+       {
+           return ResponseEntity.ok("Task with id " + id + " was updated successfully");
+       }else
+       {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task with id " + id + " does not exist");
+       }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable long id)
+    {
+        boolean isDeleted = taskService.deleteTask(id);
+
+        if(isDeleted){
+            return ResponseEntity.ok("Task with id " + id + " was deleted successfully.");
+        }else
+        {
+           return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Id " + id + " does not exist.");
+        }
+    }
 }
